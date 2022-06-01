e5afd2ad5c61b14468c757e17f485b78ed7f3a96
Brenda309 <brewihogora23@gmail.com>
2022-06-01 17:52:05 +0200

    Add the to do stracture

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import './index.css';
+
+const form = document.getElementById('form');
+const tasks = [
+  {
+    desc: 'microverse recap',
+    completed: true,
+    index: 0,
+  },
+  {
+    desc: 'microverse recap',
+    completed: false,
+    index: 1,
+  },
+  {
+    desc: 'microverse recap',
+    completed: false,
+    index: 0,
+  },
+];
+
+for (let i = 0; i < tasks.length; i += 1) {
+  const newCheckbox = document.createElement('input');
+  newCheckbox.setAttribute('type', 'checkbox');
+  newCheckbox.setAttribute('id', 'checkbox');
+  const newLabel = document.createElement('label');
+  newLabel.setAttribute('for', 'checkbox');
+  newLabel.innerHTML = tasks[i].desc;
+  const lineBreak = document.createElement('br')
+  const line = document.createElement('hr');
+  form.appendChild(line);
+  form.appendChild(lineBreak);
+  form.appendChild(newCheckbox);
+  form.appendChild(newLabel);
+}
