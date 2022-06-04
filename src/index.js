1121a6ca3d8e8a8fee5d0fc86e6d2ae8a0cfffbb
Brenda309 <brewihogora23@gmail.com>
2022-06-04 10:10:11 +0200

    Fix the linter errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-/* eslint-disable*/
 import './index.css';
-import {Tasks, addList,display} from './functions';
+import { Tasks, addList, display } from './functions.js';
 import Todos from './class.js';
-import { update } from 'lodash';
-// import { update } from 'lodash';
-
 
 const storage = new Tasks();
 
-  if (localStorage.Tasks) {
-    storage.array = JSON.parse(localStorage.Tasks);
-  }
+if (localStorage.Tasks) {
+  storage.array = JSON.parse(localStorage.Tasks);
+}
 
 addList.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
@@ -25,42 +21,40 @@ addList.addEventListener('keypress', (e) => {
     window.location.reload();
   }
 });
-//display items
+// display items
 display();
 
-const remove = document.querySelector('.trashIcon');
-for (let i = 0; i < storage.array.length; i += 1){
-const rmv = document.getElementById(`rmv${i}`);
-const descriptionName =  storage.array[i].description;
-const deleteItem = document.getElementById(`item${i}`);
-rmv.addEventListener('click', () => {
-const filtered = storage.array.filter((Tasks) => Tasks.description !== descriptionName);
-const stringData = JSON.stringify(filtered);
-localStorage.setItem('Tasks', stringData);
-deleteItem.remove();
-window.location.reload();
-
-})
-}
-
- const updateIndex = () => {
-  storage.array.forEach((todos, index) => {
-todos.index = index + 1;
-  })
-const stringData = JSON.stringify(storage.array);
+for (let i = 0; i < storage.array.length; i += 1) {
+  const rmv = document.getElementById(`rmv${i}`);
+  const descriptionName = storage.array[i].description;
+  const deleteItem = document.getElementById(`item${i}`);
+  rmv.addEventListener('click', () => {
+    const filtered = storage.array.filter((Tasks) => Tasks.description !== descriptionName);
+    const stringData = JSON.stringify(filtered);
     localStorage.setItem('Tasks', stringData);
-   }
- updateIndex();
- 
+    deleteItem.remove();
+    window.location.reload();
+  });
+}
+
+const updateIndex = () => {
+  storage.array.forEach((todos, index) => {
+    todos.index = index + 1;
+  });
+  const stringData = JSON.stringify(storage.array);
+  localStorage.setItem('Tasks', stringData);
+};
+updateIndex();
+
 const updateTask = () => {
-const list = document.getElementsByClassName('list');
-for (let i = 0; i < list.length; i += 1){
-  list[i].addEventListener('change', () => {
-    storage.array[i].description = list[i].value;
-    const stringData = JSON.stringify(storage.array);
-        localStorage.setItem('tasks', stringData)
-        window.location.reload();
-  })
-}
-}
+  const list = document.getElementsByClassName('list');
+  for (let i = 0; i < list.length; i += 1) {
+    list[i].addEventListener('change', () => {
+      storage.array[i].description = list[i].value;
+      const stringData = JSON.stringify(storage.array);
+      localStorage.setItem('tasks', stringData);
+      window.location.reload();
+    });
+  }
+};
 updateTask();
